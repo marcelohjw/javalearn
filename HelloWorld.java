f8c456cc44864fb87cb1ca72a63af37a09c3a439
Marcelo Junior <marcelohjw@gmail.com>
2022-04-03 03:08:39 -0300

    pilot stuff

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+class HelloWorld {
+    public static void main (String[] args) {
+        System.out.println("Oi!");
+    }
+}
+
+class HelloWorld {
+    public static void main (String[] args) {
+        System.out.println("Oi!");
+    }
+}
