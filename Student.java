f46cf766aead499c2b2c1d4f974fea5669a19f02
Marcelo Junior <marcelohjw@gmail.com>
2022-04-03 14:02:03 -0300

    Primitive Variables, started study

--- Student.java
+++ Student.java
@@ -1,10 +1,16 @@
 class Student {
     int id = 1000;
+    byte age = 18;
+    short rank = 165;
+    long phone = 987654321;
     int nextId = id + 1;
 
     void compute() {
         System.out.println("Id: " + id);
         System.out.println("Next Id: " + nextId);
+        System.out.println("Age: " + age);
+        System.out.println("Rank: " + rank);
+        System.out.println("Phone: " + phone);
     }
 
     public static void main (String[] args) {
