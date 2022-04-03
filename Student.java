0e80bd4e83a9544eaf93383e85124ab28f6f8761
Marcelo Junior <marcelohjw@gmail.com>
2022-04-03 16:17:00 -0300

    Primitive Variables: Integers + Demo done

--- Student.java
+++ Student.java
@@ -2,8 +2,11 @@ class Student {
     int id = 1000;
     byte age = 18;
     short rank = 165;
-    long phone = 987654321;
+    long phone = 987_654_321l;
     int nextId = id + 1;
+
+    int minValue = Integer.MIN_VALUE;
+    int maxValue = Integer.MAX_VALUE;
 
     void compute() {
         System.out.println("Id: " + id);
@@ -11,6 +14,8 @@ class Student {
         System.out.println("Age: " + age);
         System.out.println("Rank: " + rank);
         System.out.println("Phone: " + phone);
+        System.out.println("Minimum Value: " + minValue);
+        System.out.println("Maximum Value: " + maxValue);
     }
 
     public static void main (String[] args) {
