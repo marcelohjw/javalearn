74cafc5e5288a81ec26a2a7192080ca6715511a0
Marcelo Junior <marcelohjw@gmail.com>
2022-04-04 04:21:44 -0300

    Primitive Variables: Floting-point Numbers + Demo

--- Student.java
+++ Student.java
@@ -3,19 +3,15 @@ class Student {
     byte age = 18;
     short rank = 165;
     long phone = 987_654_321l;
-    int nextId = id + 1;
 
-    int minValue = Integer.MIN_VALUE;
-    int maxValue = Integer.MAX_VALUE;
+    double gpa = 3.8;
 
     void compute() {
         System.out.println("Id: " + id);
-        System.out.println("Next Id: " + nextId);
         System.out.println("Age: " + age);
         System.out.println("Rank: " + rank);
         System.out.println("Phone: " + phone);
-        System.out.println("Minimum Value: " + minValue);
-        System.out.println("Maximum Value: " + maxValue);
+        System.out.println("GPA: " + gpa);
     }
 
     public static void main (String[] args) {
