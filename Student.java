4d96a6318c32556ce8e9416d341b928267a942dc
Marcelo Junior <marcelohjw@gmail.com>
2022-04-05 20:31:40 -0300

    Primitive Variables: Character Data Type +

--- Student.java
+++ Student.java
@@ -6,12 +6,15 @@ class Student {
 
     double gpa = 3.8;
 
+    char degree = '\u0042'; //Unicode for B
+
     void compute() {
         System.out.println("Id: " + id);
         System.out.println("Age: " + age);
         System.out.println("Rank: " + rank);
         System.out.println("Phone: " + phone);
         System.out.println("GPA: " + gpa);
+        System.out.println("Degree: " + degree);
     }
 
     public static void main (String[] args) {
