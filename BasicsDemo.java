07be86dd5edfaa0a4cec17c51a4c7ffd43a20225
Marcelo Junior <marcelohjw@gmail.com>
2022-04-21 18:26:51 -0300

    Arrays + Demo done

--- BasicsDemo.java
+++ BasicsDemo.java
@@ -64,22 +64,52 @@ class BasicsDemo{
         byte bByte = 65;
         charA = (char)bByte; // Special convetion (widening from byte --> int followed by narrowing from int --> char)
         System.out.println("charA: " + charA);
-        
-
-
     }
 
+    static void arrays() {
+        System.out.println("Inside arrays..\n");
+        /*
+        int scores[] = new int[4];
+        scores[0] = 90;
+        scores[1] = 70;
+        scores[2] = 80;
+        scores[3] = 100;
+        */
+
+        //int scores[] = new int[] {90, 70, 80, 100};
+
+        int scores[] = {90, 70, 80, 100};
+
+        System.out.println("Mid-Term 1: " + scores[0]);
+        System.out.println("Mid-Term 2: " + scores[1]);
+        System.out.println("Mid-Term Final: " + scores[2]);
+        System.out.println("Mid-Term Project: " + scores[3]);
+        System.out.println("Exams: " + scores.length);
+
+
+        /* On windows will work
+        Student[] students = new Student[3];
+        students[0] = new Student();
+        students[1] = new Student();
+        students[2] = new Student();
+        System.out.println("Student 1: " + students[0]);
+        System.out.println("Student 2: " + students[1]);
+        System.out.println("Student 3: " + students[2]);
+        */
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("Primitives: ");
-        primitives();
-        System.out.println("---------------------");
-        System.out.println(1 - 0.9);
+        //System.out.println("Primitives: ");
+        //primitives();
+        //System.out.println("---------------------");
+        //System.out.println(1 - 0.9);
 
-        BigDecimal _first = new BigDecimal("0.1");
-        BigDecimal second = new BigDecimal("0.2");
-        System.out.println(_first.add(second));
-        typeCasting();
-
+        //BigDecimal _first = new BigDecimal("0.1");
+        //BigDecimal second = new BigDecimal("0.2");
+        //System.out.println(_first.add(second));
+        //typeCasting();
+        arrays();
     }
 
 }
--- assignement.java
+++ assignement.java
@@ -6,19 +6,18 @@ class CurrencyConverter {
     int mexican_peso = 18;       
     int _yen = 107;
     int $australian = 2;  
-    int dollar;
-    int Rupee = 63;
+
+    double exchangeRates[] = {rupee, dirham, real, chilean_peso, mexican_peso, _yen, $australian};
 
     void printCurrencies () {
-        System.out.println("rupee: " + rupee);
-        System.out.println("dirham: " + dirham);   
-        System.out.println("real: " + real);   
-        System.out.println("chilean_peso: " + chilean_peso);   
-        System.out.println("mexican_peso: " + mexican_peso);   
-        System.out.println("_yen: " + _yen);   
-        System.out.println("$australian: " + $australian);
-        System.out.println("dollar: " + dollar);
-        System.out.println("Rupee: " + Rupee);
+        System.out.println("rupee: " + exchangeRates[0]);
+        System.out.println("dirham: " + exchangeRates[1]);   
+        System.out.println("real: " + exchangeRates[2]);   
+        System.out.println("chilean_peso: " + exchangeRates[3]);   
+        System.out.println("mexican_peso: " + exchangeRates[4]);   
+        System.out.println("_yen: " + exchangeRates[5]);   
+        System.out.println("$australian: " + exchangeRates[6]);
+        System.out.println("Currency's Number: " + exchangeRates.length);
 
     }
 
