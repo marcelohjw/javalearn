6d436216a3c24109a7aeff7de66918845c248650
Marcelo Junior <marcelohjw@gmail.com>
2022-04-25 05:40:16 -0300

    Little bit of standalone work for study the assignement stuff

--- assignement.java
+++ assignement.java
@@ -1,13 +1,18 @@
 class CurrencyConverter {
-    int rupee = 63;
-    int dirham = 3; 
-    int real = 3;     
-    int chilean_peso = 595;
-    int mexican_peso = 18;       
-    int _yen = 107;
-    int $australian = 2;  
 
-    double exchangeRates[] = {rupee, dirham, real, chilean_peso, mexican_peso, _yen, $australian};
+    double[] exchangeRates;
+
+    void setExchangeRates(double[] rates) {
+        exchangeRates = rates;
+    }
+
+    void updateExchangeRates(int arrayIndex, double newVal) {
+        exchangeRates[arrayIndex] = newVal;
+    }
+
+    double getExchangeRate(int arrayIndex) {
+        return exchangeRates[arrayIndex];
+    }
 
     void printCurrencies () {
         System.out.println("rupee: " + exchangeRates[0]);
@@ -23,6 +28,18 @@ class CurrencyConverter {
 
     public static void main (String[] args) {
         CurrencyConverter cc = new CurrencyConverter();
+
+        
+        double[] ratex = {63.0, 3.0, 3.0, 595.0, 18.0, 107.0, 2.0};
+        cc.setExchangeRates(ratex);
         cc.printCurrencies();
+        cc.updateExchangeRates(2, 4.5); 
+        System.out.println("\nUpdating real money..\n");
+        cc.printCurrencies();
+        System.out.println("\nGetting chilean peso..\n");
+        double get = cc.getExchangeRate(3);
+        System.out.println(get);
+
+
     }
 }
