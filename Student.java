82aac2a31b9e3415a17bbbbcb04333883f7395fb
Marcelo Junior <marcelohjw@gmail.com>
2022-05-08 19:11:40 -0300

    Constructors + Demoinitiated

--- Student.java
+++ Student.java
@@ -10,6 +10,28 @@ class Student {
     boolean international;
     double tuitionFees = 12000.0, internationalFees = 5000.0;
 
+    Student(int newId, 
+            String newName, 
+            String newGender,
+            int newAge, 
+            long newPhone, 
+            double newGpa, 
+            char newDegree, 
+            boolean isInternational) {
+        System.out.println("CONSTRUCTOR ON");
+        System.out.println("----------------");
+        id = newId;
+        name = newName;
+        gender = newGender;
+        age = newAge;
+        phone = newPhone;
+        gpa = newGpa;
+        degree = newDegree;
+        international = isInternational;
+    }
+
+    Student() {}
+
     void compute() {
         computeCount = computeCount + 1;
         if (international) {
@@ -27,39 +49,31 @@ class Student {
     }
 
     public static void main (String[] args) {
-        Student student1 = new Student();
-        // Sem constructor
-        student1.id = 1000;
-        student1.name = "Marcelo";
-        student1.gender = "male";
-        student1.age = 18;
-        student1.phone = 987_654_321l;
-        student1.gpa = 3.8;
-        student1.degree = '\u0042'; // Unicode for B
-        student1.international = false;
+        Student student1 = new Student( 
+                                        1000,
+                                        "Marcelo",
+                                        "male",
+                                        18,
+                                        987_654_321l,
+                                        3.8,
+                                        '\u0042', // Unicode for B
+                                        false
+                                       );
         student1.compute();
 
-        Student student2 = new Student();
-        // Sem constructor
-        student2.id = 1001;
-        student2.name = "Maya";
-        student2.gender = "female";
-        student2.age = 20;
-        student2.phone = 983_652_323l;
-        student2.gpa = 3.5;
-        student2.degree = '\u0046'; // Unicode
-        student2.international = true;
+        Student student2 = new Student(
+                                        1001,
+                                        "Maya",
+                                        "female",
+                                        28,
+                                        983_652_323l,
+                                        3.5,
+                                        '\u0046',
+                                        true
+        );
         student2.compute();
 
         Student student3 = new Student();
-        student3.id = 1002;
-        student3.name = "Anita";
-        student3.gender = "female";
-        student3.age = 21;
-        student3.phone = 223_456_8888L;
-        student3.gpa = 4.0;
-        student3.degree = 'M';
-        student3.international = true;
         student3.compute();
 
         System.out.println("Student compute count: " + Student.computeCount);
