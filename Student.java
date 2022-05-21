3b995194e64cb61c9dad023cc6b4d304b4c6e011
Marcelo Junior <marcelohjw@gmail.com>
2022-05-21 05:08:54 -0300

    Constructors Overloading + Demo Part 1 done

--- Student.java
+++ Student.java
@@ -10,14 +10,8 @@ class Student {
     boolean international;
     double tuitionFees = 12000.0, internationalFees = 5000.0;
 
-    Student(int newId, 
-            String newName, 
-            String newGender,
-            int newAge, 
-            long newPhone, 
-            double newGpa, 
-            char newDegree, 
-            boolean isInternational) {
+    /** PARAMETRIZED CONSTRUCTOR */
+    Student(int newId, String newName, String newGender,int newAge, long newPhone, double newGpa, char newDegree, boolean isInternational) {
         System.out.println("----------------");
         System.out.println("CONSTRUCTOR ON");
         id = newId;
@@ -45,6 +39,29 @@ class Student {
         System.out.println("Student Number: " + studentCount + "\n");
     }
 
+    Student(int newId, String newName, String newGender,int newAge, long newPhone, double newGpa, char newDegree) {
+        System.out.println("----------------");
+        System.out.println("CONSTRUCTOR 2 ON");
+        id = newId;
+        name = newName;
+        gender = newGender;
+        age = newAge;
+        phone = newPhone;
+        gpa = newGpa;
+        degree = newDegree;
+
+        studentCount = studentCount + 1;
+        System.out.println("Id: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Gender: " + gender);
+        System.out.println("Age: " + age);
+        System.out.println("Phone: " + phone);
+        System.out.println("GPA: " + gpa);
+        System.out.println("Degree: " + degree);
+        System.out.println("tuitionFees: " + tuitionFees);
+        System.out.println("Student Number: " + studentCount + "\n");
+    }
+
     Student() {}
 
     void compute() {
@@ -53,7 +70,7 @@ class Student {
 
     public static void main (String[] args) {
         System.out.println("STUDENTS\n");
-        Student student1 = new Student(1000, "Marcelo", "male", 18, 987_654_321l, 3.8, '\u0042', false);
+        Student student1 = new Student(1000, "Marcelo", "male", 18, 987_654_321l, 3.8, '\u0042');
 
         Student student2 = new Student(1001, "Maya", "female", 28, 983_652_323l, 3.5, '\u0046', true);
 
