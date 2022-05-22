610b95492987d537e005f5119a71d7c59efdb0fe
Marcelo Junior <marcelohjw@gmail.com>
2022-05-22 04:50:52 -0300

    Demo: this reference done

--- Student.java
+++ Student.java
@@ -11,23 +11,25 @@ class Student {
     double tuitionFees = 12000.0, internationalFees = 5000.0;
 
     /** PARAMETRIZED CONSTRUCTOR */
-    Student(int newId, String newName, String newGender,int newAge, long newPhone, double newGpa, char newDegree) {
-        this(newId, newName, newGender, newAge, newPhone, newGpa, newDegree, false);
+    Student(int id, String name, String gender,int age, long phone, double  gpa, char degree) {
+        this(id, name, gender, age, phone, gpa, degree, false); // Invocation Statement
         System.out.println("----------------");
         System.out.println("SECONDARY CONSTRUCTOR\n");
+        System.out.println("----------------");
+
     }
 
-    Student(int newId, String newName, String newGender,int newAge, long newPhone, double newGpa, char newDegree, boolean isInternational) {
+    Student(int id, String name, String gender,int age, long phone, double  gpa, char degree, boolean international) {
         System.out.println("----------------");
         System.out.println("PRIMARY CONSTRUCTOR");
-        id = newId;
-        name = newName;
-        gender = newGender;
-        age = newAge;
-        phone = newPhone;
-        gpa = newGpa;
-        degree = newDegree;
-        international = isInternational;
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+        this.phone = phone;
+        this.gpa = gpa;
+        this.degree = degree;
+        this.international = international;
 
         studentCount = studentCount + 1;
         if (international) {
@@ -36,24 +38,40 @@ class Student {
         }
 
         studentCount = studentCount + 1;
-        System.out.println("Id: " + id);
-        System.out.println("Name: " + name);
-        System.out.println("Gender: " + gender);
-        System.out.println("Age: " + age);
-        System.out.println("Phone: " + phone);
-        System.out.println("GPA: " + gpa);
-        System.out.println("Degree: " + degree);
+        System.out.println("Id: " + this.id);
+        System.out.println("Name: " + this.name);
+        System.out.println("Gender: " + this.gender);
+        System.out.println("Age: " + this.age);
+        System.out.println("Phone: " + this.phone);
+        System.out.println("GPA: " + this.gpa);
+        System.out.println("Degree: " + this.degree);
         System.out.println("tuitionFees: " + tuitionFees);
         System.out.println("Student Number: " + studentCount + "\n");
     }
 
+    boolean updateProfile(String name) {
+        this.name = name;
+        return true;
+    }
+
     public static void main (String[] args) {
-        System.out.println("STUDENTS\n");
+        System.out.println("STUDENTS INFO\n");
         Student student1 = new Student(1000, "Marcelo", "male", 18, 987_654_321l, 3.8, '\u0042');
 
         Student student2 = new Student(1001, "Maya", "female", 28, 983_652_323l, 3.5, '\u0046', true);
 
         Student student3 = new Student(1002, "Anitta", "female", 29, 444_669_767l, 3.0, '\u0040', true);
+
+        System.out.println("\nNAMES");
+        System.out.println("Student 1: " + student1.name);
+        System.out.println("Student 2: " + student2.name);
+        System.out.println("Student 3: " + student3.name);
+
+        student1.updateProfile("John");
+
+        System.out.println("Student 1: " + student1.name);
+
+
 
         System.out.println("Student count: " + Student.studentCount);
 
